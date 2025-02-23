55e72e0a2094c83b07788d245b067fb3409e12bb
Daniel <daniel.mutembei@student.moringaschool.com>
2025-02-23 19:34:28 +0300

    done

--- lib/models.py
+++ lib/models.py
@@ -12,6 +12,8 @@ from sqlalchemy import (CheckConstraint, UniqueConstraint,
 
 from sqlalchemy.ext.declarative import declarative_base
 
+sys.path.append(os.getcwd())
+
 engine = create_engine('sqlite:///migrations_test.db')
 
 Base = declarative_base()
@@ -26,7 +28,14 @@ class Student(Base):
     birthday = Column(DateTime())
     enrolled_date = Column(DateTime(), default=datetime.now())
 
+    __table_args__ = (
+        UniqueConstraint('email', name='unique_email'),
+        CheckConstraint('grade >= 1 AND grade <= 12', name='grade_between_1_and_12')
+    )
     def __repr__(self):
         return f"Student {self.id}: " \
             + f"{self.name}, " \
             + f"Grade {self.grade}"
+    
+    if __name__ == "__main__":
+        Base.metadata.create_all(engine)
